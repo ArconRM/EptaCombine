8ae480ca57f2afad266e5a8c74731d15c869e677
a <85395885+ArconRM@users.noreply.github.com>
2025-07-23 22:59:23 +0400

    Added migrations to git

---
+++ LatexCompiler/Migrations/20250723183642_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LatexCompiler.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LatexProjects",
+                columns: table => new
+                {
+                    Uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ProjectDirectory = table.Column<string>(type: "text", nullable: false),
+                    MainTexName = table.Column<string>(type: "text", nullable: false),
+                    MainBibName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LatexProjects", x => x.Uuid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LatexProjects");
+        }
+    }
+}
