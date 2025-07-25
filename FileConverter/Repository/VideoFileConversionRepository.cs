42fe9df5ba9e8818d879df885159a488c67044da
a <85395885+ArconRM@users.noreply.github.com>
2025-07-25 11:49:14 +0400

    Fixed video converter

--- FileConverter/Repository/VideoFileConversionRepository.cs
+++ FileConverter/Repository/VideoFileConversionRepository.cs
@@ -5,7 +5,7 @@ using FileConverter.Repository.Interfaces;
 
 namespace FileConverter.Repository;
 
-public class VideoFileConversionRepository: IVideoFileConversionRepository
+public class VideoFileConversionRepository : IVideoFileConversionRepository
 {
     public async Task<Stream> ConvertVideoAsync(
         Stream inputStream,
@@ -15,29 +15,52 @@ public class VideoFileConversionRepository: IVideoFileConversionRepository
     {
         var tempInputFile = Path.GetTempFileName();
         var tempOutputFile = Path.GetTempFileName();
-        
+
         try
         {
             await using (var fileStream = File.Create(tempInputFile))
             {
                 await inputStream.CopyToAsync(fileStream, token);
             }
-            
-            var args = FFMpegArguments
-                .FromFileInput(tempInputFile)
-                .OutputToFile(tempOutputFile, true, opts => opts
-                    .WithVideoCodec(VideoCodec.LibX264)
-                    .WithAudioCodec(AudioCodec.Aac)
-                    .ForceFormat(outFormat));
-            
-            await args.ProcessAsynchronously(throwOnError: true);
-            
+
+            switch (outFormat)
+            {
+                case "gif":
+                    await FFMpegArguments
+                        .FromFileInput(tempInputFile)
+                        .OutputToFile(tempOutputFile, true, opts => opts
+                            .WithCustomArgument(
+                                "-filter_complex \"fps=10,scale=360:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\"")
+                            .WithCustomArgument("-loop 0")
+                            .ForceFormat(outFormat))
+                        .ProcessAsynchronously(throwOnError: true);
+                    break;
+                case "webm":
+                    await FFMpegArguments
+                        .FromFileInput(tempInputFile)
+                        .OutputToFile(tempOutputFile, overwrite: true, opts => opts
+                            .WithVideoCodec(VideoCodec.LibVpx)
+                            .WithAudioCodec(AudioCodec.LibVorbis)
+                            .ForceFormat(outFormat))
+                        .ProcessAsynchronously(throwOnError: true);
+                    break;
+                default:
+                    await FFMpegArguments
+                        .FromFileInput(tempInputFile)
+                        .OutputToFile(tempOutputFile, true, opts => opts
+                            .WithVideoCodec(VideoCodec.LibX264)
+                            .WithAudioCodec(AudioCodec.Aac)
+                            .ForceFormat(outFormat))
+                        .ProcessAsynchronously(throwOnError: true);
+                    break;
+            }
+
             var output = new MemoryStream();
             await using (var fileStream = File.OpenRead(tempOutputFile))
             {
                 await fileStream.CopyToAsync(output, token);
             }
-            
+
             output.Position = 0;
             return output;
         }
